9a47d487bb5fbb8d3c761bf5acf04f2e33d2468f
Andrea Zavodska <deorwynn@gmail.com>
2018-09-16 14:45:19 +0200

    added slider and countdown

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -10,7 +10,24 @@ import SideDrawer from './SideDrawer';
 class Header extends Component {
 
     state = {
-        drawerOpen: false
+        drawerOpen: false,
+        headerShow: false
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        if(window.scrollY > 0) {
+            this.setState({
+                headerShow: true
+            })
+        } else {
+            this.setState({
+                headerShow: false
+            })
+        }
     }
 
     toggleDrawer = (value) => {
@@ -26,12 +43,12 @@ class Header extends Component {
                 style={{
                     boxShadow: 'none',
                     padding: '10px 0',
-                    backgroundColor: '#2f2f2f'
+                    backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent'
                 }}
             >
                 <Toolbar>
                     <div className="header_logo">
-                        <div className="font_righteous header_logo_venue">Exota</div>
+                        <div className="font_righteous header_logo_venue">Exotica</div>
                         <div className="header_logo_title">Exotic Bird Show</div>
                     </div>
 
