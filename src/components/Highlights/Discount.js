0b84ac28a66ff581fcf29ff4da1a3377891a9c95
Andrea Zavodska <deorwynn@gmail.com>
2018-09-20 22:07:25 +0200

    added button component

--- src/components/Highlights/Discount.js
+++ src/components/Highlights/Discount.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
+import MyButton from '../UI/MyButton';
 
 class Discount extends Component {
 
@@ -42,7 +43,12 @@ class Discount extends Component {
                             <h3>Purchase tickets before 1st November</h3>
                             <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
                         
-                            <div>Button</div>
+                            <MyButton
+                                text="Purchase tickets"
+                                bck="#ffa800"
+                                color="#fff"
+                                link="http://google.com"
+                            />
                         
                         </div>
                     </Slide>
